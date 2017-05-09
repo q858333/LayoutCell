1935388eab47c55d1734b9666faf46a51404544b
dengbin <leo_dengbin@sina.com>
2017-05-09 11:29:38 +0800

    搭建整体ui

---
+++ LayoutCell/LayoutCell/Login/DBLoginVC.m
@@ -0,0 +1,55 @@
+//
+//  DBLoginVC.m
+//  LayoutCell
+//
+//  Created by dengbin on 17/5/9.
+//  Copyright © 2017年 dengbin. All rights reserved.
+//
+
+#import "DBLoginVC.h"
+#import "DBMainTabBarController.h"
+@interface DBLoginVC ()
+
+@end
+
+@implementation DBLoginVC
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+- (IBAction)loginButtonClick:(id)sender {
+
+
+    NSArray *titleArr = @[@"最近",@"联系人",@"功能",@"设置"];
+    NSArray *classArr = @[@"",@"",@"",@""];
+
+
+    Class class = NSStringFromClass(<#Class  _Nonnull __unsafe_unretained aClass#>)
+
+
+
+
+
+
+}
+
+- (IBAction)registerButtonClick:(id)sender {
+}
+@end
